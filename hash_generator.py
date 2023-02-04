950e86dcb1fff676d0ebffed0b856e73fb404ee4
Ivan Nikitovic <ivan.bnikitovic@gmail.com>
2023-02-04 18:43:16 -0500

    add hash generator functionality

---
+++ hash_generator.py
@@ -0,0 +1,49 @@
+import random
+
+class HashGenerator():
+
+    def __init__(self, p: int, k: int) -> None:
+        """ 
+        Initializes Hash class.
+
+        Parameters
+        ----------
+        p: integer
+            Choose a primer integer p such that p >= n,
+            where n is the range of possible inputs in universe.
+            (e.g. for ASCII: p >= 128)
+
+        k: integer
+            Input dimension. For strings, this would be the max
+            input string length. Shorter strings are padded.
+
+        """
+        self.p = p
+        self.k = k
+
+    def generate_hash_function(self) -> None:
+        """
+        Generates hash function as follows:
+            select k integers {z1, ..., zk} by U.A.R. sampling 
+            k times from the set {0, ..., p - 1}
+
+        """
+        self.z = [random.randint(0, self.p - 1) for i in range(self.k)]
+
+    def hash(self, s: str) -> int:
+        """
+        Hashes input string as follows:
+        h(a1, ..., ak) = (SUM_overall zi * ai) mod p
+
+        Parameters
+        ----------
+        s: string
+            Must be less than self.k.
+
+        """
+        assert(len(s) < self.k)
+
+        s = s.rjust(self.k, " ")
+        emb = [ord(ch) for ch in s]
+
+        return sum(map(lambda x: x[0] * x[1], zip(self.z, emb))) % self.p
