f2f117ed278aaf3917fba1d4970fb605db086cf2
Ivan Nikitovic <ivan.bnikitovic@gmail.com>
2023-02-05 20:07:42 -0500

    added Markov estimate

--- experiment2.py
+++ experiment2.py
@@ -22,6 +22,7 @@ N = sum(actual_freq.values())
 
 print(f"Number of unique words: {len(actual_freq)}")
 print(f"Number of words, total: {N}")
+print()
 
 sigma = 0.005
 w = ceil(math.e / sigma) # 544
@@ -31,11 +32,15 @@ delta = 0.05
 h = ceil(math.log(1/delta)) # 3
 print(f"height: {h}")
 
+print()
+
 # estimate less than sigma * N with probability 1 - delta:
 # a_pred < a_act + 129.23 with probability 0.95
 
 err = sigma * N
-print(f"estimate less than {err} with probability {1 - delta}")
+print(f"Markov In.: error less than {2 * N / w} with probability: 0.5")
+print(f"CMS Theory: error less than {err} with probability: {1 - delta}")
+print()
 
 cms_shakespeare = CountMinSketch(w, h) # 544 * 3
 cms_shakespeare.process_stream(corpus)
@@ -57,7 +62,7 @@ sorted_freqs = sorted(error_freq.items(), key=lambda x: x[0])
 X, Y = list(zip(*sorted_freqs))
 
 acc = sum([pair[1] for pair in sorted_freqs if pair[0]<err])/sum(Y)
-print(f"actual estimate accuracy: {acc}")
+print(f"actual error less than {err} with probability: {acc}")
 
 plt.scatter(X, Y)
 plt.show()
